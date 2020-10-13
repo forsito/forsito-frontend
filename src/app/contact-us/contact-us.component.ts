075300b1a4a6b707d24693b4cedd66075635df29
TarekMahmoud175 <tarek64@live.com>
2020-10-13 16:45:42 +0200

    Finshed form using reactive approach

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ContactUs: FormGroup;
 
   ngOnInit(): void {
+    this.ContactUs = new FormGroup({
+      name: new FormControl(null),
+      email: new FormControl(null),
+      mobile: new FormControl(null),
+      msg: new FormControl(null),
+    });
   }
-
+  onSubmit() {
+    console.log(this.ContactUs.value);
+  }
 }
