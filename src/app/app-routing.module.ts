07d16f5e6d56307b4ea86abeda77a356cae7e09f
TarekMahmoud175 <tarek64@live.com>
2020-10-15 16:09:31 +0200

    added routing to navbar , header button (home page) styling buttons remains icons , project and profile modals

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CrewComponent } from './crew/crew.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+  // home
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  // crew
+  { path: 'crew', component: CrewComponent },
+  // projects
+  { path: 'projects', component: ProjectsComponent },
+  // contact us
+  { path: 'contact-us', component: ContactUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
