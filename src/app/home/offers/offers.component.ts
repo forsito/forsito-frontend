181f9c067f1cd2a97f9efff660cc0e4674445e1d
Hossam Sherif <husam287@hotmail.com>
2020-11-06 14:02:02 +0200

    Some Small Changes in home images
    -chnage their size
    -change the domain
    -correct some spelling

--- src/app/home/offers/offers.component.ts
+++ src/app/home/offers/offers.component.ts
@@ -7,12 +7,13 @@ import { Offer } from 'src/app/models/offer.model';
   styleUrls: ['./offers.component.css']
 })
 export class OffersComponent implements OnInit {
-offers:Offer[] = 
-[{'id': 1 , 'image':'../../../assets/img/fastIcon.png' , 'name':'fast Work'} ,
-{'id': 2 , 'image':"../../../assets/img/Technical support.png" , 'name':'Technical support'},
-{'id': 3 , 'image':"../../../assets/img/Responsive.png" , 'name':'Responsive'}
-]
-;
+  offers: Offer[] =
+    [
+      { 'id': 1, 'image': '../../../assets/img/fastIcon.png', 'name': 'Fast Work' },
+      { 'id': 2, 'image': "../../../assets/img/Technical support.png", 'name': 'Technical support' },
+      { 'id': 3, 'image': "../../../assets/img/Responsive.png", 'name': 'Responsive' }
+    ]
+    ;
   constructor() { }
 
   ngOnInit(): void {
