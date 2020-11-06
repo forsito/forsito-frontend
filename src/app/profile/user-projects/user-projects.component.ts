b7643f0d70131d3d848e797647545cc04bddcbe0
TarekMahmoud175 <tarek64@live.com>
2020-11-06 13:05:39 +0200

    added forsito icon to the project added (no projects yet) if user have no project

--- src/app/profile/user-projects/user-projects.component.ts
+++ src/app/profile/user-projects/user-projects.component.ts
@@ -4,23 +4,20 @@ import { Crew } from 'src/app/models/crew.model';
 import { CrewService } from 'src/app/services/crew.service';
 import { ProjectService } from 'src/app/services/project.service';
 
-
 @Component({
   selector: 'app-user-projects',
   templateUrl: './user-projects.component.html',
   styleUrls: ['./user-projects.component.css'],
 })
 export class UserProjectsComponent implements OnInit {
-
   id: string;
   userProjects: { _id: string; title: string }[];
-
-
+  length: number;
 
   constructor(
     private route: ActivatedRoute,
     private crewService: CrewService,
-    private projectService: ProjectService,
+    private projectService: ProjectService
   ) {}
 
   /*================================================================================== */
@@ -34,7 +31,8 @@ export class UserProjectsComponent implements OnInit {
     this.id = this.route.snapshot.parent.params.id;
     this.crewService.getUserByID(this.id).subscribe((result: Crew) => {
       this.userProjects = result.projects.slice();
+      this.length = this.userProjects.length;
+      console.log(this.length);
     });
   }
 }
-
